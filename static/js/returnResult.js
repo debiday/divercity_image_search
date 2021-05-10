5195069fe2c8ca2e5ad2296ee217961f854c0829
Deborah <deborah.oyt@gmail.com>
2021-05-10 05:43:08 +0000

    Remove testing from homepage

--- static/js/returnResult.js
+++ static/js/returnResult.js
@@ -3,29 +3,14 @@
 console.log("i am saving input!");
 
 function returnResults(evt) {
-    console.log('hello search function');
 
     evt.preventDefault();
-
-//     let today = new Date();
-//     let date_saved = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
-
-    // //Get email
-    // let email = $('#user-email').text()
-
-    // //Get user_id
-    // let user_id = $('#user-id').text()
 
 
     let url = "/save-city";
 
     let savedData = {
-                    //  'user_id': user_id,
                      'city': $('#user-city').val(),
-                //      'user_id': $('#user-id').text(),
-                //      'date_saved': date_saved,
-                //      'notes': $('#user-city').val(),
-                    //  'email': email
                     };
 
 
@@ -34,23 +19,18 @@ function returnResults(evt) {
 
         let final_url = response;
 
-        // let url_front = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=4f2a9c7f2ea592f840664a1486e37348&text=%22";
-        // let url_back = "+people%22&per_page=100&format=json&nojsoncallback=1";
-        // let final_url = url_front.concat(city, url_back);
-
 
         let settings = {
                         "async": true,
                         "crossDomain": true,
                         "url": final_url,
-                        //"url": "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=4f2a9c7f2ea592f840664a1486e37348&text=%22london+people%22&per_page=100&format=json&nojsoncallback=1",
                         "method": "GET",
                         "headers": {}
                         }
 
 
         $.ajax(settings).done(function (data) {
-        console.log(data);
+
 
         document.getElementById('flickr').replaceChildren();
  
@@ -61,13 +41,9 @@ function returnResults(evt) {
                 let serverId = gp.server;
                 let id = gp.id;
                 let secret = gp.secret;
-        // let button_html = "<input type="radio" value="email">"
 
 
         console.log(farmId + ", " + serverId + ", " + id + ", " + secret);
-        //  https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-        //https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg
-        //https://www.flickr.com/photos/155854489@N04/51154725669/
 
         
         $("#flickr").append('<a href="https://www.flickr.com/photo.gne?id=' + id + '/" target="_blank"><img src="https://farm' + farmId + '.staticflickr.com/' + serverId + '/' + id + '_' + secret + '.jpg"/></a><input class="form-check-input" type="checkbox" name="selected" value="https://www.flickr.com/photo.gne?id=' + id + '/" target="_blank">');
