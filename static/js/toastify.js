acc3583b4f234d86847df118a2abd6fe17a80674
Deborah <Deborah.oyt@gmail.com>
2021-06-13 17:34:02 -0700

    Create toastify.js

---
+++ static/js/toastify.js
@@ -0,0 +1,37 @@
+"use strict";
+
+document.querySelector('#login-button').addEventListener('click', (evt) => {
+    // preventDefault will prevent the default behavior so be flexible with the functionality
+    evt.preventDefault();
+
+    const formData = {
+        email: $('#email').val(),
+        password: $('#password').val()
+    };
+
+    $.post('/login', formData, (res) => {
+        if (res === "Not found") {
+            Toastify({
+                text: "An account for this email doesn't exist yet.",
+                duration: 2000,
+                backgroundColor: "linear-gradient(to right, #874006, #330C04)",
+                className: "info",
+                }).showToast();
+        }
+        else if (res === 'Wrong password') {
+            Toastify({
+                text: 'Wrong password. Please try again.',
+                duration: 2000,
+                backgroundColor: "linear-gradient(to right, #874006, #330C04)"
+                }).showToast();
+        }
+        else if (res === 'Logged in') {
+            Toastify({
+                text: 'Logged in!',
+                duration: 2000,
+                backgroundColor: "linear-gradient(to right, #BB6606, #693B08)"
+                }).showToast();
+                setTimeout(() => {window.location.href='/user-page'}, 2000);
+        }
+    })
+});
